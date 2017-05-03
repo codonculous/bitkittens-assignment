bd195b191e7977cd215e44b3236de5712db09e4e
codonculous <feidameng01@gmail.com>
2017-05-03 16:05:17 -0400

    use animate method to make cats appear at different locations

--- cats.js
+++ cats.js
@@ -10,13 +10,24 @@ $(function(){
     }).done(function(data){
       console.log('http: ' + data.cats[0].photo);
       var catsDiv = [];
+      var catsBook = [];
       for (var i = 0; i<data.cats.length; i++) {
-        var catImg = $("<img>",{src:data.cats[i].photo, alt:'Phote of '+data.cats[i].name});
+        $('#cats-book').append('<li>'+data.cats[i].name+'</li>');
+        var catImg = $("<img>",{src:data.cats[i].photo, alt:'Phote of '+ data.cats[i].name});
         catsDiv.push($("<div>",{id:'cat'+(i+1), class:'cat-box'}));
-        catsDiv[i].appendTo($('main'));
         catImg.appendTo(catsDiv[i]);
+        $(catsDiv[i]).animate({left:move()+'px',top:move()+'px'},'slow');
       }
+      $('#cat-home').html(catsDiv);
+
     });
+
+  function move(){
+    var newM = Math.floor(Math.random() * (400 - 100)) + 100;
+    return newM;
+  }
+
+
 
 
 

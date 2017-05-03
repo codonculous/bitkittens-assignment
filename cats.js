1bab45059c7d35c8cc8e0edb96d50de6ce3307fe
codonculous <feidameng01@gmail.com>
2017-05-03 14:07:51 -0400

    change number cats ajax can request

--- cats.js
+++ cats.js
@@ -6,12 +6,15 @@ $(function(){
       method:'GET',
       url: 'http://bitkittens.herokuapp.com/cats.json',
       dataType:'json',
-      // date:{}
+      data:{'number': '5'}
     }).done(function(data){
       console.log('http: ' + data.cats[0].photo);
+      var catsDiv = [];
       for (var i = 0; i<data.cats.length; i++) {
         var catImg = $("<img>",{src:data.cats[i].photo, alt:'Phote of '+data.cats[i].name});
-        catImg.appendTo('.cat-box');
+        catsDiv.push($("<div>",{id:'cat'+(i+1), class:'cat-box'}));
+        catsDiv[i].appendTo($('main'));
+        catImg.appendTo(catsDiv[i]);
       }
     });
 

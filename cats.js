f0c9b9f5026fed9750e1c994a909f56668993d7c
codonculous <feidameng01@gmail.com>
2017-05-03 13:21:45 -0400

    create ajax method to make get data in json from http://bitkittens.herokuapp.com/cats.json

--- cats.js
+++ cats.js
@@ -2,6 +2,12 @@ $(function(){
 
   $('main button.summon-cats').click(function(){
     console.log('clicked');
+    $.ajax({
+      method:'GET',
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      dataType:'json',
+      // date:{}
+    });
 
   });
 

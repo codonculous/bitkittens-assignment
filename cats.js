77a2a8cbd3b191d2986d29b4a68b9fc99223d428
codonculous <feidameng01@gmail.com>
2017-05-03 13:47:59 -0400

    use ajax to return and display the pictures of cat photo on index from http://bitkittens.herokuapp.com/cats.json

--- cats.js
+++ cats.js
@@ -7,7 +7,15 @@ $(function(){
       url: 'http://bitkittens.herokuapp.com/cats.json',
       dataType:'json',
       // date:{}
+    }).done(function(data){
+      console.log('http: ' + data.cats[0].photo);
+      for (var i = 0; i<data.cats.length; i++) {
+        var catImg = $("<img>",{src:data.cats[i].photo, alt:'Phote of '+data.cats[i].name});
+        catImg.appendTo('.cat-box');
+      }
     });
+
+
 
   });
 
